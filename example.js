e2b7758165c3bbcf448844b0f6862c73cf32ca69
matiasvlevi <matiasvlevi@gmail.com>
2021-01-01 16:42:22 -0500

    Update example.js

--- example.js
+++ example.js
@@ -1,4 +1,17 @@
 const { makeData } = require('./index.js');
 
-let data = makeData(6);
-console.log(data);
+// Creating a 3-bit +1 Counting dataset
+    let data = makeData(3);
+    console.log(data);
+
+// Create a 5-bit 2x logic rule data.
+    // let data = makeData(5,function(x) {
+    //     return 2*x;
+    // });
+    // console.log(data);
+
+// Create a 6-bit x^2 logic rule data.
+    // let data = makeData(6,function(x) {
+    //     return x*x;
+    // });
+    // console.log(data);
