8af43a77840d04b6ec8be9fbe5fcb36ef72c9df2
matiasvlevi <matiasvlevi@gmail.com>
2021-01-01 16:45:20 -0500

    Update example.js

--- example.js
+++ example.js
@@ -1,16 +1,17 @@
 const { makeData } = require('./index.js');
 
-// Creating a 3-bit +1 Counting dataset
+// Example 1: Creating a 3-bit +1 Counting dataset.
     let data = makeData(3);
     console.log(data);
 
-// Create a 5-bit 2x logic rule data.
+// Example 2: Create a 5-bit 2x logic rule data.
     // let data = makeData(5,function(x) {
     //     return 2*x;
     // });
     // console.log(data);
 
-// Create a 6-bit x^2 logic rule data.
+
+// Example 3: Create a 6-bit x^2 logic rule data.
     // let data = makeData(6,function(x) {
     //     return x*x;
     // });
